ca3f52cf385b0e7ea73b3bd960f28e2dd99a7405
Shawn Haupage <111591051+shawnhaupage@users.noreply.github.com>
2023-02-22 18:54:31 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,24 +2,31 @@
 <html>
 <head>
 	<title>Pet Breeding Schedule</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- Add link to Bootstrap stylesheet -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
 </head>
 <body>
-	<h1>Pet Breeding Schedule</h1>
-	<form method="post" action="schedule.php">
-		<label for="breed">Breed:</label>
-		<input type="text" name="breed" id="breed">
-		<br>
-		<label for="male">Male:</label>
-		<input type="text" name="male" id="male">
-		<br>
-		<label for="female">Female:</label>
-		<input type="text" name="female" id="female">
-		<br>
-		<label for="date">Date:</label>
-		<input type="date" name="date" id="date">
-		<br>
-		<input type="submit" value="Schedule Breeding">
-	</form>
+	<div class="container">
+		<h1 class="text-center mb-5">Pet Breeding Schedule</h1>
+		<form method="post" action="schedule.php">
+			<div class="form-group">
+				<label for="breed">Breed:</label>
+				<input type="text" name="breed" id="breed" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="male">Male:</label>
+				<input type="text" name="male" id="male" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="female">Female:</label>
+				<input type="text" name="female" id="female" class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="date">Date:</label>
+				<input type="date" name="date" id="date" class="form-control">
+			</div>
+			<button type="submit" class="btn btn-primary">Schedule Breeding</button>
+		</form>
+	</div>
 </body>
 </html>

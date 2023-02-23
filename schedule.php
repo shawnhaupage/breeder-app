bc9aa218f4ec521232b587c63bdc3798178f04f3
Shawn Haupage <111591051+shawnhaupage@users.noreply.github.com>
2023-02-22 18:57:57 -0600

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -1,23 +1,53 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Pet Breeding Schedule</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<?php
-		$breed = $_POST['breed'];
-		$male = $_POST['male'];
-		$female = $_POST['female'];
-		$date = $_POST['date'];
-	?>
-	<h1>Pet Breeding Schedule</h1>
-	<p>Your breeding has been scheduled:</p>
-	<ul>
-		<li>Breed: <?php echo $breed; ?></li>
-		<li>Male: <?php echo $male; ?></li>
-		<li>Female: <?php echo $female; ?></li>
-		<li>Date: <?php echo $date; ?></li>
-	</ul>
-</body>
-</html>
+<?php
+// Check if the form was submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	// Get the form values
+	$breed = isset($_POST['breed']) ? trim($_POST['breed']) : '';
+	$male = isset($_POST['male']) ? trim($_POST['male']) : '';
+	$female = isset($_POST['female']) ? trim($_POST['female']) : '';
+	$date = isset($_POST['date']) ? trim($_POST['date']) : '';
+
+	// Validate the form values
+	$errors = [];
+	if (empty($breed)) {
+		$errors[] = 'Breed is required.';
+	}
+	if (empty($male)) {
+		$errors[] = 'Male is required.';
+	}
+	if (empty($female)) {
+		$errors[] = 'Female is required.';
+	}
+	if (empty($date)) {
+		$errors[] = 'Date is required.';
+	} else if (!preg_match('/\d{4}-\d{2}-\d{2}/', $date)) {
+		$errors[] = 'Invalid date format. Please use the format YYYY-MM-DD.';
+	}
+
+	// If there are no errors, save the schedule
+	if (empty($errors)) {
+		// Connect to the database
+		$host = 'your-database-host';
+		$username = 'your-database-username';
+		$password = 'your-database-password';
+		$dbname = 'your-database-name';
+		$conn = new mysqli($host, $username, $password, $dbname);
+
+		// Check the connection
+		if ($conn->connect_error) {
+			die('Connection failed: ' . $conn->connect_error);
+		}
+
+		// Insert the schedule into the database
+		$sql = "INSERT INTO schedules (breed, male, female, date) VALUES ('$breed', '$male', '$female', '$date')";
+		if ($conn->query($sql) === TRUE) {
+			$message = 'Schedule saved successfully.';
+		} else {
+			$message = 'Error: ' . $sql . '<br>' . $conn->error;
+		}
+
+		// Close the database connection
+		$conn->close();
+	}
+}
